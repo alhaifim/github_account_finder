a0b05c1d2d8416dab503b0433e3138bed578a1a6
alhaifim <alhaifim@outlook.com>
2019-11-22 13:23:08 -0700

    Added axios to via node to connect the git api, connected to github api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar.component';
 import Users from './components/users/users.component';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+state = {
+  users: [],
+  loading: false
+}
+async componentDidMount(){
+  this.setState({loading: true});
+  const resp = await axios.get('https://api.github.com/users');
+  this.setState({users: resp.data, loading: false});
+}
+
   render() {
     return (
       <div className='App'>
         <Navbar/>
         <div className="container">
-        <Users/>
+        <Users users={this.state.users} loading={this.state.loading}/>
         </div>
         
       </div>
